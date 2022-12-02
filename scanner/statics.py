a6a9b05178d2bfeb587d868cd4352285e3e83464
thedrna <dornadehghani@ymail.com>
2022-12-02 13:31:16 +0330

    DFA

--- scanner/statics.py
+++ scanner/statics.py
@@ -19,6 +19,7 @@ class Common(enum.Enum):
     EOF = 'EOF'
     N_OF_STATES = 19
     FINAL_STATES = [2, 4, 5, 6, 8, 9, 13, 15, 16, 18]
+    STAR_STATES = [2, 4, 5, 9, 16, 18]
 
 
 class Transitions(enum.Enum):
@@ -27,20 +28,28 @@ class Transitions(enum.Enum):
     LETTER = 'a-zA-Z'
     NOT_LETTER = '~(a-zA-Z)'
 
+
 class CharType(enum.Enum):
     DIGIT = '[0-9]'
-    NOT_DIGIT = '[^0-9]'  
+    NOT_DIGIT = '[^0-9]'
     LETTER = '[a-zA-Z]'
     NOT_LETTER = '[^a-zA-Z]'
     DIGIT_LETTER = '[a-zA-Z0-9]'
     NOT_DIGIT_LETTER = '[^a-zA-Z0-9]'
     SYMBOL_NEQUAL_NSTAR = 'SYMBOL_NEQUAL_NSTAR'
+    SINGLE_SYMBOL = ';|:|,|\[|\]|\(|\)|{|}|\+|-|<'
     WHITESPACE = '\s|\n|\t|\f|\r|\v'
     EOF = 'EOF'
+    EOF_ENTER = 'EOF|\n'
     EQUAL = '='
+    NEQUAL = '^='
     STAR = '*'
+    NSTAR = '^*'
     SLASH = '/'
+    NSLASH = '^/'
+    NSTAR_NSLASH = '^/*'
     INVALID = 'INVALID'
+
 
 tokens = {
     TokenType.KEYWORD: ['if', 'else', 'void', 'int', 'while', 'break', 'switch', 'default', 'case', 'return', 'endif'],
