5be1cc499cd3488812fa9beabb985963a05bedab
thedrna <dornadehghani@ymail.com>
2022-12-02 16:57:23 +0330

    check ID and KEYWORD fixed

--- scanner/writer.py
+++ scanner/writer.py
@@ -18,7 +18,8 @@ class Writer:
             for k in key_words:
                 f.writelines(f'{key_words.index(k) + 1}: {k}\n')
             for token in symbols_table:
-                f.writelines(f'{symbols_table.index(token) + len(key_words) + 1}: {token}\n')
+                if token not in key_words:
+                    f.writelines(f'{symbols_table.index(token) + len(key_words) + 1}: {token}\n')
 
 
 if __name__ == "__main__":
@@ -26,7 +27,7 @@ if __name__ == "__main__":
     #               ('SYMBOL', '{')],
     #           2: [('KEYWORD', 'int'), ('ID', 'a'), ('SYMBOL', '='), ('NUM', '0'), ('SYMBOL', ';')]}
 
-    symbols = ['a', 'b', 'cde']
+    symbols = ['a', 'b', 'cde', 'if']
     writer = Writer()
     # writer.write_tokens(tokens)
     writer.write_symbols(symbols)
